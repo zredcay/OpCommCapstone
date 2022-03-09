95e49bfa6505c14ca08eaf58f63379df928937cf
linuxMachine <zredcay@ycp.edu>
2022-03-09 14:27:37 -0500

    trying with hard float arm

--- Bluetooth.py
+++ Bluetooth.py
@@ -1,9 +1,12 @@
 import serial
-ser = serial.Serial('/dev/ttyS0', timeout=1, baudrate=115000)
-ser.flushInput();
-ser.flushOutput();
+import bluetooth
+#ser = serial.Serial('/dev/ttyS0', timeout=1, baudrate=115000)
+#ser.flushInput();
+#ser.flushOutput();
    
-while True:
-    out = ser.readline().decode()
-    if out!='' : print (out)
-    if out == 'exit': break
+#while True:
+#    out = ser.readline().decode()
+ #   if out!='' : print (out)
+ #   if out == 'exit': break
+with open('/dev/ttyS0','w',1) as f:
+     f.write('\'hello from python!')
