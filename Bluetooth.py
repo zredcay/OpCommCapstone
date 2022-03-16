730586465aecdfe4955b87a601fadc0817b0154b
PI1 <zredcay@ycp.edu>
2022-03-16 11:32:52 -0400

    fixed the archtecture issue with the pi needed to compile on pi directly

--- Bluetooth.py
+++ Bluetooth.py
@@ -1,5 +1,5 @@
 import serial
-ser = serial.Serial('/dev/ttyS0', timeout=1, baudrate=115000)
+ser = serial.Serial('/dev/rfcomm0', timeout=1, baudrate=115000)
 ser.flushInput();
 ser.flushOutput();
    
@@ -7,3 +7,5 @@ while True:
     out = ser.readline().decode()
     if out!='' : print (out)
     if out == 'exit': break
+#with open('/dev/ttyS0','w',1) as f:
+#     f.write('hello from python!')
