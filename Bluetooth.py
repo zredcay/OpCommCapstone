e4717d898660408be970772adb24ae7c53810e98
linuxMachine <zredcay@ycp.edu>
2022-03-16 13:01:43 -0400

    updated to commit

--- Bluetooth.py
+++ Bluetooth.py
@@ -1,12 +1,12 @@
 import serial
-import bluetooth
-#ser = serial.Serial('/dev/ttyS0', timeout=1, baudrate=115000)
+#import bluetooth
+#ser = serial.Serial('/dev/rfcomm0', timeout=1, baudrate=115000)
 #ser.flushInput();
 #ser.flushOutput();
    
 #while True:
 #    out = ser.readline().decode()
- #   if out!='' : print (out)
- #   if out == 'exit': break
-with open('/dev/ttyS0','w',1) as f:
+#    if out!='' : print (out)
+#    if out == 'exit': break
+with open('/dev/rfcomm0','w',1) as f:
      f.write('\'hello from python!')
