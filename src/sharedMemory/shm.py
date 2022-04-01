1efa7738107ce7af4a72a7e55a69b54f1d4ebe39
zredcay <zredcay@ycp.edu>
2022-04-01 15:18:48 -0400

    completed the semaphore implementation for shm

--- src/sharedMemory/shm.py
+++ src/sharedMemory/shm.py
@@ -5,14 +5,21 @@ import sys
 
 def shmwrite(data, flag):
     # old way of doing things
-    # create key for source
+    # create key for shm
     ipc_key = 111111
+    
+    # create name for semaphore
+    sem_name = 'opcapstone'
+    
     # create memory object
     memory = sysv_ipc.SharedMemory(ipc_key, flags = 0)
-    sem_name = 'opcapstone'
-    sem = posix_ipc.Semaphore(sem_name, 0, 0o600, 1)
-    
-    try:      
+        
+    try:
+        # create sem object using the name created in main.c
+        sem = posix_ipc.Semaphore(sem_name, 0, 0o600, 1)
+        
+        # acquire access to the semaphore with a timeout of 1 s
+        sem.acquire(1)
         # if flag is 0 switch to source segment
         if flag == 0:
             # convert string to bytes
@@ -28,6 +35,4 @@ def shmwrite(data, flag):
             # write to memory
             memory.write(bytes_data, offset = 36)
     finally:
-        sem.release(1)
-    
-shmwrite('1', 0)
+        sem.release()
