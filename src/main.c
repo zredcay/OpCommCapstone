2bf6ce38d8306aa6617a9aaffbe9a2a5a6ba2c24
linuxMachine <zredcay@ycp.edu>
2022-04-04 14:28:41 -0400

    added to math function to be able to return a usable transciever from Imu data and rplidar data

--- src/main.c
+++ src/main.c
@@ -13,13 +13,88 @@
 #include <errno.h>
 #include "sharedMemory.h"
 #include <semaphore.h>
+#include <math.h>
 
-int math(float Ax, float Ay, float Az, float Gx, float Gy, float Gz, float Mx, float My, float Mz, int trans, float angle)
+int convertTransceiver(int trans)
 {
+	int newtrans;
 	
+	if( trans == 0)
+	{
+		newtrans = 1;
+	}
+	else if (trans == 1)
+	{
+		newtrans = 0;
+	}
+	else if (trans == 2)
+	{
+		newtrans = 7;
+	}
+	else if (trans == 3)
+	{
+		newtrans = 6;
+	}
+	else if (trans == 4)
+	{
+		newtrans = 5;
+	}
+	else if (trans == 5)
+	{
+		newtrans = 4;
+	}
+	else if (trans == 6)
+	{
+		newtrans = 3;
+	}
+	else if (trans == 7)
+	{
+		newtrans = 2;
+	}
 
 
-return 0;
+	return newtrans;
+}
+struct mainData math(float Ax, float Ay, float Az, float Mx, float My, float Mz, float timer, struct mainData ex)
+{
+
+	struct mainData data = ex;
+	printf(" angle %f distance %f transciever %d Velocity %f %f\n", data.angle, data.dist, data.trans, data.Vx, data.Vy);
+	float PI  = 3.14159265;
+	 float val = PI / 180.0;
+	
+	float Vfx = data.Vx + (Ax * timer);
+	float Vfy = data.Vy + (Ay * timer);
+	float Xo = data.dist*cos(data.angle*val); 
+	float Yo = data.dist*sin(data.angle*val);
+	float Xf = Xo + .5*(Vfx + data.Vx)*timer;
+	float Yf = Yo + .5*(Vfy + data.Vy)*timer;
+	
+	printf("Vfx %f Vfy %f Xo %f Yo %f Xf %f Yf %f \n", Vfx, Vfy, Xo, Yo, Xf, Yf);
+	
+	
+	data.angle = (atan(Yf/Xf))/val;
+	if(Xf < 0 )
+	{ 
+		data.angle = data.angle + 180;
+	}
+	if(Yf < 0 && Xf > 0)
+	{
+		data.angle = data.angle + 360;
+	}
+	data.dist = sqrt((Xf * Xf) + (Yf * Yf));
+	
+	data.trans = convertTransceiver(data.angle/45);
+	
+	data.Vx = Vfx;
+	data.Vy = Vfy;
+	
+	
+	
+	printf(" angle %f distance %f transciever %d Velocity %f %f\n",  data.angle, data.dist, data.trans, data.Vx, data.Vy);
+	
+	
+return data;
 }
 
 
@@ -29,20 +104,40 @@ int main ()
 	char message[100];
 	int n = 0;
 	int test;
-	float Ax, float Ay, float Az, float Gx, float Gy, float Gz, float Mx, float My, float Mz;
-	float angle;
-	float dist;
-	int transceiver;
+	float Ax = .2;
+	float Ay = .1; 
+	float Az; float Gx; float Gy; float Gz; float Mx; float My; float Mz;
+	float angle = 100;
+	float dist = .3; 
+	int transceiver = 0;
 	int result;	
+	float mainTimer = .5;
+	float Vx = 0;
+	float Vy = 0;
 	
 	struct data ex;
 	struct shared sharMem;
 	struct Memory arr;
+	struct mainData data;
 	
 	
 	sem_t* mutex;
 	
+	data.angle = 120;
 	
+	data.dist = .2;
+	
+	data.trans = 3;
+	
+	data.Vx = -.2;
+	data.Vy = -.1;
+	
+	
+	
+	for(int i = 0; i < 4; i++){
+	 data = math(Ax, Ay, Az, Mx, My, Mz, mainTimer, data);
+	 }
+	 exit(-1);
 	
 	
 	State NextState = Intialization;
@@ -77,6 +172,7 @@ int main ()
      	  				//use this to convert to the data  
      				}
      			//transceiver = math(Ax, Ay, Az, Gx, Gy, Gz, Mx, My, Mz, transceiver, angle);
+     			//ata = math(Ax, Ay, Az, Mx, My, Mz, transceiver, dist, angle, mainTimer, Vx, Vy);
      			//************end of maintenance code**********
             		NewEvent = Code_Finished_Event;
 			NextState = CodeFinishedHandler(NextState);
