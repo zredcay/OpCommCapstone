41817796a1bee65575047e8065f5c312f34a0578
PI2 <zredcay@ycp.edu>
2022-04-25 18:09:09 -0400

    added src offset vals

--- src/bluetooth/BluetoothServer.py
+++ src/bluetooth/BluetoothServer.py
@@ -2,7 +2,7 @@ from bluedot.btcomm import BluetoothServer  # imports the Bluetooth Server libra
 from signal import pause  # imports pause function from signal allowing program to idle until a connection is created
 import sys
 sys.path.append('/home/pi/OpCommCapstone/src/imu')
-import IMU
+import SRCIMU
 sys.path.append('/home/pi/OpCommCapstone/src/sharedMemory')
 import shm
 
@@ -12,9 +12,9 @@ def data_received(data):  # defines the data_received function which is passed t
     # write jeff's data to shm
     shm.shmwrite(data, 1)
     
-    accx, accy, accz = IMU.getAcc()  # break the acceleration tuple into its respective parts
-    gyrox, gyroy, gyroz = IMU.getGyro()  # break the gyro tuple into its respective parts
-    magx, magy, magz = IMU.getMagnetic()  # break the magnetic tuple into its respective parts
+    accx, accy, accz = SRCIMU.getAcc()  # break the acceleration tuple into its respective parts
+    gyrox, gyroy, gyroz = SRCIMU.getGyro()  # break the gyro tuple into its respective parts
+    magx, magy, magz = SRCIMU.getMagnetic()  # break the magnetic tuple into its respective parts
     print(accx,accy,accz)
     # assign data to val variable
     val = str(accx) + ' ' + str(accy) + ' ' + str(accz) + ' ' + str(gyrox) + ' ' + str(gyroy) + ' ' + str(gyroz) + ' ' + str(magx) + ' ' + str(magy) + ' ' + str(magz)
---
+++ src/imu/SRCIMU.py
@@ -0,0 +1,45 @@
+# SPDX-FileCopyrightText: Copyright (c) 2020 Bryan Siepert for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+import time
+import board
+import adafruit_lsm6ds
+import adafruit_lis3mdl
+from adafruit_lsm6ds import Rate, AccelRange, GyroRange
+
+
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+lsm = adafruit_lsm6ds.LSM6DSOX(i2c)
+lis = adafruit_lis3mdl.LIS3MDL(i2c)
+lsm.accelerometer_range = AccelRange.RANGE_2G
+accxoff = 0.0359
+accyoff = 0.0837
+acczoff = 9.9129
+gyroxoff = 0.5425
+gyroyoff = 0.3194
+gyrozoff = 0.1269
+magxoff = 52.6966
+magyoff = 12.3063
+magzoff = 26.0596
+
+def getAcc():
+    accx, accy, accz = lsm.acceleration\
+    accx -= accxoff
+    accy -= accyoff
+    accz -= acczoff
+    return accx, accy, accz
+
+def getGyro():
+    gyrox, gyroy, gyroz = lsm.gyro
+    gyrox -= gyroxoff
+    gyroy -= gyroyoff
+    gyroz -= gyrozoff
+    return gyrox, gyroy, gyroz
+
+def getMagnetic():
+    magx, magy, magz = lis.magnetic
+    magx -= magxoff
+    magy -= magyoff
+    magz -= magzoff
+    return magx, magy, magz
