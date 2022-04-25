1c5b68a5a20752c211cba690cfc37d93d03ee319
PI2 <zredcay@ycp.edu>
2022-04-25 17:44:47 -0400

    imu offset code and calibration

--- src/imu/IMU.py
+++ src/imu/IMU.py
@@ -5,22 +5,41 @@ import time
 import board
 import adafruit_lsm6ds
 import adafruit_lis3mdl
+from adafruit_lsm6ds import Rate, AccelRange, GyroRange
+
 
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 lsm = adafruit_lsm6ds.LSM6DSOX(i2c)
 lis = adafruit_lis3mdl.LIS3MDL(i2c)
-
+lsm.accelerometer_range = AccelRange.RANGE_2G
+accxoff =
+accyoff =
+acczoff =
+gyroxoff =
+gyroyoff =
+gyrozoff =
+magxoff =
+magyoff =
+magzoff =
 
 def getAcc():
-    accx, accy, accz = lsm.acceleration
+    accx, accy, accz = lsm.acceleration\
+    accx -= accxoff
+    accy -= accyoff
+    accz -= acczoff
     return accx, accy, accz
 
 def getGyro():
     gyrox, gyroy, gyroz = lsm.gyro
+    gyrox -= gyroxoff
+    gyroy -= gyroyoff
+    gyroz -= gyrozoff
     return gyrox, gyroy, gyroz
 
 def getMagnetic():
     magx, magy, magz = lis.magnetic
+    magx -= magxoff
+    magy -= magyoff
+    magz -= magzoff
     return magx, magy, magz
-
