30c12526b7f129f78fd20e87d756562a2c16bf13
PI2 <zredcay@ycp.edu>
2022-04-27 19:01:38 -0400

    asdf

--- src/main.c
+++ src/main.c
@@ -565,7 +565,7 @@ int main () {
 
                             //printf("MESSAGE: %s\n", rec_msg);
 
-                            printf("MSG REC: %s\n",rec_msg);
+                            //printf("MSG REC: %s\n",rec_msg);
 
                             // checksum calculation
                             int checksum = 0;
@@ -584,8 +584,8 @@ int main () {
 
                                 if ((result = strcmp(rec_msg, prev_msg)) != 0){
                                     if ((result = strcmp(rec_msg,disc_msg)) != 0){
-                                        printf("PREV MSG REC: %s\n",prev_msg);
-                                        printf("DIFFERENT MSG REC: %s\n",rec_msg);
+                                        //printf("PREV MSG REC: %s\n",prev_msg);
+                                        //printf("DIFFERENT MSG REC: %s\n",rec_msg);
                                         for (int k = 0; k <= 6; k++){
                                             prev_msg[k] = rec_msg[k];
                                         }
